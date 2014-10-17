9e401f91f10e6ba257d7ebb8e4db107061842d31
Jaspreet <jbagga@sfu.ca>
2014-10-17 10:19:28 -0700

    Changed names to TSMDBManager

---
+++ TSMDBManager.h
@@ -0,0 +1,33 @@
+//
+//  DBManager.h
+//  MedAssist
+//
+//  Created by Jass on 2014-10-12.
+
+#ifndef MedAssist_TSMDBManager_h
+#define MedAssist_TSMDBManager_h
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
+@interface TSMDBManager : NSObject
+{
+    NSString *databasePath;
+}
+@property NSMutableArray* arrResults;
+@property NSMutableArray* arrColumnNames;
+@property NSMutableArray* arrDataRow;
++(TSMDBManager*)getSharedInstance;
+-(BOOL)createDB; //create database, and tables
+
+-(BOOL) saveUser:(NSString*)userName userId:(int64_t)userId;//save all input from input forms
+//TO DO: need to connect to app pages (UI) and object classes
+
+-(NSArray*) findByUserName:(NSString*)userName;//select all data of a user
+//TO DO: create objects of different classes and store information to display on app
+
+-(BOOL) createTableHelper:(const char*) sql_stmt errMsg:(char *)errMsg;//helper called by createDB, that monitors creation of table-error if fails
+
+-(NSArray*) getResult:(NSString*) query;//helper called by findByUserName that executes all select queries
+@end
+
+#endif
