b8ac5543b89640977be1ffc07a06655d6a8fd020
Jaspreet <jbagga@sfu.ca>
2014-10-17 18:42:37 -0700

    Added database, classes and UI

---
+++ MedAssist/TSMProfileMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSMProfileMenuViewController.h
+//  MedAssist
+//
+//  Created by Ariana Williams on 2014-10-13.
+//  Copyright (c) 2014 Team Salveo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSMProfileMenuViewController : UIViewController
+
+@end
